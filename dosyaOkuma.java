c9159fd30faeb5c9aae86f3d187d5ea340167c16
yaseminersp <yersepciler@gmail.com>
2018-03-07 22:24:50 +0300

    Second commit

---
+++ dosyaOkuma.java
@@ -0,0 +1,60 @@
+/*
+ * dosyaOkuma.java
+ * 
+ * Copyright 2018 yasemin <yasemin@yersp>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class dosyaOkuma {
+	
+	public static void main (String args[]) throws IOException {
+		
+		ArrayList<Character> harfler=new ArrayList<Character>();
+		FileReader inputStream=null;
+		
+		try{
+				inputStream=new FileReader("/home/yasemin/Desktop/text.txt");
+				
+				int con;
+				
+				while((con=inputStream.read())!=-1){
+					
+						
+						harfler.add((char)con);						
+						Set<Character> sonHarfler=new LinkedHashSet<Character>(harfler);
+						sonHarfler.remove(' ');
+						sonHarfler.remove('.');
+						System.out.println(sonHarfler);
+				}
+		}
+		
+		finally{
+				if(inputStream !=null){
+					inputStream.close();
+				}
+		}
+	
+						
+				}
+				
+}
+ 
